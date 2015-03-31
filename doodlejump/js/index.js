75527dea83b19f68f28f02b62da6a14bb1d473e1
arifchu001 <arifchu001@lionsdenhk.net>
2015-03-31 13:36:27 +0800

    hi

--- doodlejump/js/index.js
+++ doodlejump/js/index.js
@@ -133,35 +133,33 @@ function Platform() {
 		} catch (e) {}
 	};
 
+
 	//Platform types
 	//1: Normal
 	//2: Moving
 	//3: Breakable (Go through)
 	//4: Vanishable 
 	//Setting the probability of which type of platforms should be shown at what score
-	if (score >= 100) {
-		this.types = [1, 2, 3, 4];
-	}
-	//The Chances of Monsters Spawning in!!!
-	var mobSpawn = [1,2,3,4]
-	if (mobSpawn == 1) 
-
+	if (score >= 5000) this.types = [2, 3, 3, 3, 4, 4, 4, 4];
+	else if (score >= 2000 && score < 5000) this.types = [2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4];
+	else if (score >= 1000 && score < 2000) this.types = [2, 2, 2, 3, 3, 3, 3, 3];
+	else if (score >= 500 && score < 1000) this.types = [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3];
+	else if (score >= 100 && score < 500) this.types = [1, 1, 1, 1, 2, 2];
+	else this.types = [1];
 
 	this.type = this.types[Math.floor(Math.random() * this.types.length)];
 
 	//We can't have two consecutive breakable platforms otherwise it will be impossible to reach another platform sometimes!
-	if (this.type == 3 && broken >= 1) {
+	if (this.type == 3 && broken < 1) {
 		broken++;
-		} 
-		else if (this.type == 3 && broken >= 1) 
-		{
+	} else if (this.type == 3 && broken >= 1) {
 		this.type = 1;
 		broken = 0;
-		}
+	}
 
 	this.moved = 0;
 	this.vx = 1;
-}
+	}
 
 for (var i = 0; i < platformCount; i++) {
 	platforms.push(new Platform());
